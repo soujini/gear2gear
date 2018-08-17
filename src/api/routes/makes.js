50c06198827ade96179d4d2e9f68f7e176e85e5c
souj <soujanya.venkatesh@gmail.com>
2018-08-17 15:01:26 +0530

    insecure content

--- src/api/routes/makes.js
+++ src/api/routes/makes.js
@@ -9,7 +9,7 @@ const client = new Client({
   // connectionString:'postgres://localhost:5432/postgres',
   connectionString:'postgres://bltypmsejfdisv:e88eb86f18914916049a49313bb5c7a8044cd50c229e0b55671ace8e4565f4ea@ec2-174-129-236-147.compute-1.amazonaws.com:5432/d2m86fr3a5nu60?&ssl=true',
 
-  ssl: false,
+  ssl: true,
   //  database: 'postgres',
   //   user: 'postgres',
   //    port: 5432
